9293792f9fd28c4948e1206bef9ede1e7614cf98
Conner Gleason <connereg@optonline.net>
2022-07-19 13:58:02 -0400

    Completed Lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,30 @@
 import java.util.function.IntUnaryOperator;
 
 public class Main {
-	// write the nextOddNum here
+    // write the nextOddNum here
+    public static NextOddNumInter nextOddNum = (num) -> {
+        int numMutator = num+1;
+        while (numMutator % 2 == 0) {
+            numMutator += 1;
+        }
+        return numMutator;
+    };
+
     
     public static void main(String[] args) {
         int num = 5;
         int num2 = 10;
+        int num3 = 20;
 
         System.out.println(nextOddNum.applyAsInt(num)); // 7
         System.out.println(nextOddNum.applyAsInt(num2)); // 11
+        System.out.println(nextOddNum.applyAsInt(num3)); // 21
     }
 }
+
+@FunctionalInterface
+interface NextOddNumInter {
+    Integer applyAsInt(int num);
+}
+
+
